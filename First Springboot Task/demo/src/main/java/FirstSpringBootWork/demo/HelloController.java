e72844971ed62518dad536ac1196fddb07ca5d6d
NazimRaianKhan <nazimraiank@gmail.com>
2026-04-05 15:54:27 +0600

    Springboot hello world practice added

---
+++ First Springboot Task/demo/src/main/java/FirstSpringBootWork/demo/HelloController.java
@@ -0,0 +1,18 @@
+package FirstSpringBootWork.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/test")
+    public String diff() {
+        return "Hello, World!";
+    }
+}
